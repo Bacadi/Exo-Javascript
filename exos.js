cd4ae1037b91cd62557db0ee0439f2e9d0e5b50f
Bacadi <batiste.palvadeaeu42@gmail.com>
2026-09-21 10:06:47 +0200

    exercice 3

--- exos.js
+++ exos.js
@@ -7,3 +7,10 @@ console.log("Bienvenue dans le monde de la programmation !");
 const nb1 = 5;
 const nb2 = 10;
 console.log(nb1 + nb2);
+
+//EXERCICE 3
+const panier = ["comcombre", "grenade", "mûre"];
+
+panier.forEach((fruit) => {
+    console.log(fruit);
+})
