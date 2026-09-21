fdd40906f927ed3533442aca468cffe476bfcdd7
Bacadi <batiste.palvadeaeu42@gmail.com>
2026-09-21 10:04:23 +0200

    exercice 1, 2

---
+++ exos.js
@@ -0,0 +1,9 @@
+//EXERCICE 1
+console.log("Hello, world !");
+console.log("Bienvenue dans le monde de la programmation !");
+
+
+//EXERCICE 2
+const nb1 = 5;
+const nb2 = 10;
+console.log(nb1 + nb2);
