3267a1e8a15fc6de8c38b5202195d63ecc347334
Bacadi <batiste.palvadeaeu42@gmail.com>
2026-09-21 10:10:11 +0200

    exercice 4

--- exos.js
+++ exos.js
@@ -13,4 +13,11 @@ const panier = ["comcombre", "grenade", "mûre"];
 
 panier.forEach((fruit) => {
     console.log(fruit);
-})
+});
+
+//EXERCICE 4
+for(let i = 0; i <= 20; i++){
+    if(i % 2 === 0){
+        console.log(i)
+    }
+}
